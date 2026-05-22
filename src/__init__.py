73c3443728dd7d2bd53407f834e4dfc5a24d9af7
Omer Ahmed <amory30900@hotmail.com>
2026-05-23 01:15:01 +0300

    Clean up __init__.py by removing imports
    Removed unused imports from __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,5 @@
-# src/__init__.py
-# Makes src a proper Python package so imports work as `from src.pipeline import ...`
-
 from .data_prep   import prepare_data
-from .pipeline    import MentalHealthPipeline, PipelineResult, SimilarStatement
 
 __all__ = [
     "prepare_data",
-    "MentalHealthPipeline",
-    "PipelineResult",
-    "SimilarStatement",
 ]
